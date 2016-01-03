52fcb68fbaff32c82dc9ff2383ed697e230c18ee
Abhner Araújo <afga@cin.ufpe.br>
2016-01-03 02:30:25 -0300

    Insert usando procedure em add Local

--- src/model/Insert.java
+++ src/model/Insert.java
@@ -1,7 +1,10 @@
 package model;
 
+import java.sql.CallableStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.swing.JOptionPane;
 
 import views.Main;
 
@@ -18,26 +21,29 @@ public class Insert {
 	}
 	public static void insertLocal() throws SQLException{
 		String[] parametro=views.JP_Local_New.params();
-        Statement stmt = Conexao.myConn.createStatement();
-        stmt.executeUpdate("EXEC CADASTRAR_LOCAL('Home, sweet home',420,'Lugar bacaninha',4,'BobDylan','Rua Qualquer',32,'11111111','Argelia','Roscovo','Suassuna','1234,5678','Azul Celeste');");
-        /*stmt.executeUpdate("EXEC CADASTRAR_LOCAL("+ 
-        		"'"+parametro[0]+
-        		"',"+Double.parseDouble(parametro[1])+
-        		",'"+parametro[2]+
-        		"',"+Integer.parseInt(parametro[3])+
-        		",'"+Main.login.loginUsuario()+
-        		"','"+parametro[4]+
-        		"',"+Integer.parseInt(parametro[5])+
-        		",'"+parametro[6]+
-        		"','"+parametro[7]+
-        		"','"+parametro[8]+
-        		"','"+parametro[9]+
-        		"','"+parametro[10]+
-        		"','"+parametro[11]+
-        		"');");
-        */
-        stmt.close();
-        
+		try{
+		String DBCALL = "{call CADASTRAR_LOCAL(?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        CallableStatement cs = Conexao.myConn.prepareCall(DBCALL);
+        cs.setString(1, parametro[0]);
+        cs.setInt(2, Integer.parseInt(parametro[1]));
+        cs.setString(3, parametro[2]);
+        cs.setInt(4, Integer.parseInt(parametro[3]));
+        cs.setString(5, Main.login.loginUsuario());
+        cs.setString(6, parametro[4]);
+        cs.setString(7, parametro[5]);
+        cs.setInt(8, Integer.parseInt(parametro[6]));
+        cs.setString(9, parametro[7]);
+        cs.setString(10, parametro[8]);
+        cs.setString(11, parametro[9]);
+        cs.setString(12, parametro[10]);
+        cs.setString(13, parametro[11]);
+        Conexao.myConn.commit();
+        cs.executeUpdate();
+        cs.close();
+        JOptionPane.showMessageDialog(null,"Local cadastrado com sucesso!");
+		}catch(SQLException se){
+			JOptionPane.showMessageDialog(null, "Erro! Cadastro não efetuado!");
+		}
 	}
 	
 }
---
+++ src/views/Teste.java
@@ -0,0 +1,13 @@
+package views;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] params=JP_Local_New.params();
+		for (int i=0;i<params.length;i++){
+			System.out.println(params[i]);
+		}
+	}
+
+}
