17e5f60279e0e5f4f1c657a6836741933f5754f2
imlbf <mister.mox@hotmail.com>
2016-01-08 14:57:23 -0200

    Novas reservas works

--- src/views/JP_PaginaLocal.java
+++ src/views/JP_PaginaLocal.java
@@ -5,9 +5,14 @@ import java.awt.Font;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Calendar;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
@@ -16,6 +21,8 @@ import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.TitledBorder;
 
+import model.Consulta;
+import model.Insert;
 import control.Local;
 
 public class JP_PaginaLocal extends JPanel{
@@ -43,6 +50,69 @@ public class JP_PaginaLocal extends JPanel{
 		lb_rua.setText(Local.getRua());
 		lb_numero.setText(Integer.toString(Local.getNumero()));
 		lb_valor.setText(Integer.toString(Local.getValor_dia()));
+	}
+	
+	public static String formateDate(String day, String mes, String ano){
+		if(day.length() == 1){
+			day = "0" + day;
+		}
+		if(mes.length() == 1){
+			mes = "0" + mes;
+		}
+		String date = ano+"-"+mes+"-"+day+" 12:00:00.0";
+		return date;
+	}
+	
+	public static boolean checkDate(JTextField qtdReserv, JTextField diaIn, JTextField mesIn, JTextField anoIn, JTextField anoOut, JTextField diaOut, JTextField mesOut){
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+		int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+		try {
+		    if(Integer.parseInt(qtdReserv.getText()) > Local.getDisp()){
+				JOptionPane.showMessageDialog(null, "Nao ah vagas."); 
+		    }else if(Integer.parseInt(qtdReserv.getText()) <= 0) {
+		    	JOptionPane.showMessageDialog(null, "Vagas nao pode ser zero."); 
+		    }else if((Integer.parseInt(qtdReserv.getText()) < Local.getDisp()) && (Integer.parseInt(qtdReserv.getText()) > 0) ){
+		    	System.out.println("ah vagas"); 
+		    	try {
+				    if((Integer.parseInt(diaIn.getText()) <= 0) || (Integer.parseInt(mesIn.getText()) <= 0)) {
+				    	JOptionPane.showMessageDialog(null, "Os campos de data nao podem ser zero."); 
+				    } else if((Integer.parseInt(anoIn.getText()) < year)){
+				    	if(Integer.parseInt(mesIn.getText()) < month){
+				    		if(Integer.parseInt(diaIn.getText()) < day){
+				    			JOptionPane.showMessageDialog(null, "Escolha uma data no futuro."); 
+				    		}
+				    	}
+				    } else{
+				    	System.out.println("entrou na segunda data");
+				    	try {
+						    if((Integer.parseInt(diaOut.getText()) <= 0) || (Integer.parseInt(mesOut.getText()) <= 0)) {
+						    	JOptionPane.showMessageDialog(null, "Os campos de data nao podem ser zero.");
+						    
+						    } else if((Integer.parseInt(anoOut.getText()) < year) || (Integer.parseInt(anoOut.getText()) < Integer.parseInt(anoIn.getText()))){
+						    	JOptionPane.showMessageDialog(null, "Escolha uma data no futuro.");   
+						    } else if((Integer.parseInt(mesOut.getText()) < Integer.parseInt(mesIn.getText()))){ 						    
+						    	JOptionPane.showMessageDialog(null, "Escolha uma data no futuro.");   
+						    } else if((Integer.parseInt(diaOut.getText()) < Integer.parseInt(diaIn.getText()))){
+								   JOptionPane.showMessageDialog(null, "Escolha uma data no futuro.");     
+						    }else{
+						    	JOptionPane.showMessageDialog(null, "tudo vedadeiro.");
+						    	return true;
+						    }
+						} catch (Exception f) {
+							JOptionPane.showMessageDialog(null, "Data invalida."); 
+						}
+				    }
+				} catch (Exception f) {
+					JOptionPane.showMessageDialog(null, "Data invalida."); 
+				}
+		    } else {
+		    	JOptionPane.showMessageDialog(null, "Erro."); 
+		    }
+		} catch (Exception f) {
+			JOptionPane.showMessageDialog(null, "Insira um numero inteiro."); 
+		}
+		return false;
 	}
 	
 	public JP_PaginaLocal() {
@@ -165,5 +235,104 @@ public class JP_PaginaLocal extends JPanel{
 		});
 		btnCancelar.setBounds(309, 359, 89, 23);
 		add(btnCancelar);
+		
+		JTextField qtdReserv = new JTextField();
+		qtdReserv.setText("0");
+		qtdReserv.setBounds(10, 393, 30, 20);
+		add(qtdReserv);
+		qtdReserv.setColumns(10);
+		
+		JLabel qtdReservas = new JLabel("Reservas:");
+		qtdReservas.setBounds(10, 359, 150, 23);
+		add(qtdReservas);
+		
+		JLabel dataEntrada = new JLabel("Data de entrada:");
+		dataEntrada.setBounds(85, 359, 150, 23);
+		add(dataEntrada);
+		
+		JTextField diaIn = new JTextField();
+		diaIn.setText("1");
+		diaIn.setBounds(85, 393, 20, 20);
+		add(diaIn);
+		diaIn.setColumns(10);
+		
+		JLabel barra1 = new JLabel("/");
+		barra1.setBounds(110, 393, 150, 23);
+		add(barra1);
+		
+		JTextField mesIn = new JTextField();
+		mesIn.setText("1");
+		mesIn.setBounds(120, 393, 20, 20);
+		add(mesIn);
+		mesIn.setColumns(10);
+		
+		JLabel barra2 = new JLabel("/");
+		barra2.setBounds(145, 393, 150, 23);
+		add(barra2);
+		
+		JTextField anoIn = new JTextField();
+		anoIn.setText("2016");
+		anoIn.setBounds(155, 393, 40, 20);
+		add(anoIn);
+		anoIn.setColumns(10);
+		
+		JLabel dataSaida = new JLabel("Data de Saida:");
+		dataSaida.setBounds(200, 359, 150, 23);
+		add(dataSaida);
+		
+		JTextField diaOut = new JTextField();
+		diaOut.setText("1");
+		diaOut.setBounds(200, 393, 20, 20);
+		add(diaOut);
+		diaOut.setColumns(10);
+		
+		JLabel barra3 = new JLabel("/");
+		barra3.setBounds(225, 393, 150, 23);
+		add(barra3);
+		
+		JTextField mesOut = new JTextField();
+		mesOut.setText("1");
+		mesOut.setBounds(235, 393, 20, 20);
+		add(mesOut);
+		mesOut.setColumns(10);
+		
+		JLabel barra4 = new JLabel("/");
+		barra4.setBounds(260, 393, 150, 23);
+		add(barra4);
+		
+		JTextField anoOut = new JTextField();
+		anoOut.setText("2016");
+		anoOut.setBounds(270, 393, 40, 20);
+		add(anoOut);
+		anoOut.setColumns(10);
+		
+		JButton btnReservar = new JButton("Reservar");
+		btnReservar.setBounds(408, 359, 89, 23);
+		btnReservar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int year = Calendar.getInstance().get(Calendar.YEAR);
+				int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+				int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+			    boolean bol = checkDate(qtdReserv, diaIn, mesIn, anoIn, anoOut, diaOut, mesOut);
+				if(bol){
+					String dateIn = formateDate(diaIn.getText(), mesIn.getText(), anoIn.getText());
+					String dateOut = formateDate(diaOut.getText(), mesOut.getText(), anoOut.getText());
+					try {
+						Insert.insertReserva(dateIn, dateOut, Integer.parseInt(qtdReserv.getText()));
+						JOptionPane.showMessageDialog(null, "Reserva feita com sucesso."); 
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						JOptionPane.showMessageDialog(null, "Erro."); 
+					} catch (NumberFormatException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (ParseException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		add(btnReservar);
 	}
 }
